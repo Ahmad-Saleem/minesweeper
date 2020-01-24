141403fd981a8ac7aa8f313d279c0a9c6ad72941
Ahmad <it_saleem@hotmail.com>
2020-01-24 13:36:28 +0000

    build folder

---
+++ build/precache-manifest.03f7a6768ed564d19c2fcba040289770.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f43a339f190a55c52c4fac9ae345415",
+    "url": "/index.html"
+  },
+  {
+    "revision": "169b67b85939c434b110",
+    "url": "/static/css/main.2cc574fd.chunk.css"
+  },
+  {
+    "revision": "a0d4a1156cd74db545ed",
+    "url": "/static/js/2.244267ef.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.244267ef.chunk.js.LICENSE"
+  },
+  {
+    "revision": "169b67b85939c434b110",
+    "url": "/static/js/main.ff513a7b.chunk.js"
+  },
+  {
+    "revision": "72cfcfed00a552795b92",
+    "url": "/static/js/runtime-main.65dc0493.js"
+  }
+]);
